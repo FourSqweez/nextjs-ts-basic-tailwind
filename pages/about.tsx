a37049c14c28ce26cc8a01a63f7b4691cafa701c
FourSqweez <four7518@gmail.com>
2022-07-21 21:01:16 +0700

    Add : creat about components

---
+++ pages/about.tsx
@@ -0,0 +1,12 @@
+import Navbar from '../components/navbar'
+
+function about() {
+	return (
+		<>
+			<Navbar />
+			<div className="container">About page</div>
+		</>
+	)
+}
+
+export default about
